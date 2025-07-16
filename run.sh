3ed16c4bc53020a0442d888cab5ed450162b3561
Kostevych Viacheslav <tomkat.s@gmail.com>
2025-07-16 09:40:04 +0300

    fix 62

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ docker container stop $container
 docker container rm $container 
 
 docker run -dt \
+    -p 8085:300 \
     -e DB_HOST=192.168.10.5 \
     -e DB_PATH=cars_dev \
     -e DB_USER=MONITOR \
